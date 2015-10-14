53da4f6e2682c46802b42a3bc2226352b07115e7
li12242 <li12242@tju.edu.cn>
2015-10-15 00:11:02 +0800

    add install process in Polylib

--- +Polylib/install_Polylib.m
+++ +Polylib/install_Polylib.m
@@ -1,9 +1,18 @@
 localPath = pwd;
 polylibPath = fullfile(pwd, '+Polylib');
 cd(polylibPath);
+fprintf('=================================\n')
+fprintf('Start installing Polylib\n')
+fprintf('Installing of JacobiP.c\n')
 mex('-O', [polylibPath,'/JacobiP.c'], [polylibPath, '/polylib.c'])
+fprintf('Installing of zwglj.c\n')
 mex('-O', [polylibPath, '/zwglj.c'], [polylibPath, '/polylib.c'])
+fprintf('Installing of Dglj.c\n')
 mex('-O', [polylibPath, '/Dglj.c'], [polylibPath, '/polylib.c'])
+fprintf('Installing of jacobfd.c\n')
 mex('-O', [polylibPath, '/jacobfd.c'], [polylibPath, '/polylib.c'])
+fprintf('Installing of GradJacobiP.c\n')
 mex('-O', [polylibPath, '/GradJacobiP.c'], [polylibPath, '/polylib.c'])
+fprintf('=================================\n')
+fprintf('Finish installation Polylib\n')
 cd(localPath);
