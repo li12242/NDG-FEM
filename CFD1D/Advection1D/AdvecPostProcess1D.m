a85ecad3e00976a35fcef860bd7ff7b10c2c949a
li12242 <li12242@tju.edu.cn>
2015-10-24 19:25:03 +0800

    clean dir

--- CFD1D/Advection1D/AdvecPostProcess1D.m
+++
@@ -1,4 +0,0 @@
-figure('color', 'w');
-hold on
-plot(x(:), u(:))
-plot(x(:), sin(x(:)), 'ro')
--- CFD1D/LinearConvection/LinConPostProcess.m
+++
@@ -1,5 +0,0 @@
-function LinConPostProcess(mesh, c)
-% 1D Linear Convection Post Process
-plot(mesh.x, c, 'o')
-
-end% func
