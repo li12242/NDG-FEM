5de838abae9591082ce8fa5228e0147ac52cfcd7
li12242 <li12242@tju.edu.cn>
2016-03-19 00:10:10 +0800

    temp modify

--- +Utilities/+Limiter/+Limiter1D/DisDetector.m
+++ +Utilities/+Limiter/+Limiter1D/DisDetector.m
@@ -2,6 +2,7 @@ function disFlag = DisDetector(mesh, var, u)
 % Reference: 
 %   [1]: Krivodonova 2004
 
+% discontinuity detector
 % Input:
 %   mesh - mesh object
 %   var - detecroted variable
@@ -10,8 +11,8 @@ function disFlag = DisDetector(mesh, var, u)
 %   disFlag - bool flag for mesh with discontinuity
 
 uM = u(mesh.vmapM); 
-vM = var(mesh.vmapM); % local speed & local variable
-vP = var(mesh.vmapP);
+% get boundary value and neighbour's 
+vM = var(mesh.vmapM); vP = var(mesh.vmapP);
 
 disFlag = zeros(size(mesh.nx));
 
@@ -21,16 +22,17 @@ inflowFlag = (mesh.nx.*uM < 0 );
 disFlag(inflowFlag) = abs(vM(inflowFlag) - vP(inflowFlag));
 
 % mean value
-varmean = max(abs(var));
-varmean = [varmean; varmean];
+varMean = mesh.Shape.VandMatrix\var;
+varMean = [varMean(1,:); varMean(1,:)];
 
-varFlag = (varmean <= eps);
+% mean value
+varFlag = (varMean <= eps);
 
 h = mesh.x(end, :) - mesh.x(1, :);
 h = [h; h]; % element length
 % h = ones(size(mesh.nx));
 
-disFlag = disFlag./(h.^((mesh.Shape.nOrder+1)/2).*varmean );
+disFlag = disFlag./(h.^((mesh.Shape.nOrder+1)/2).*varMean );
 disFlag(varFlag) = 0;
 disFlag = sum(disFlag, 1);
 end% func
