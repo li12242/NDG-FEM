7754e0bf9076dcfd11258119ee804deedb156885
li12242 <li12242@tju.edu.cn>
2016-04-18 10:01:11 +0800

    wet/dry identify of flux calculation is corrected

---
+++ SWE1D/PostProcess/DamBreak/DamBreakWetVerify.m
@@ -0,0 +1,34 @@
+function DamBreakWetVerify
+filename = 'SWE1D.nc';
+x = ncread(filename, 'x');
+bedElevation = zeros(size(x));
+
+time = ncread(filename, 'time');
+
+FinalTime = [4, 8, 12];
+
+for itime = 1:numel(FinalTime)
+    [~, index] = min(abs(time - FinalTime(itime)));
+    fprintf('time deviation： %f\n', time(index) - FinalTime(itime));
+    h = ncread(filename, 'h', [1, index],[inf, 1]);
+    q = ncread(filename, 'q', [1, index],[inf, 1]);
+    
+    load(['DamBreakWet', num2str(FinalTime(itime)), 'mat']);
+    
+    % draw picture
+    figure; subplot(3,1,1);
+    plot(x, h, '-b.'); hold on;
+    plot(x1, h1, 'k')
+    plot(x, bedElevation, 'k')
+
+    subplot(3,1,2);
+    plot(x, q, '-b.'); hold on;
+    plot(x1, q1, 'k')
+    
+    subplot(3,1,3);
+    u = q./h; u(h<eps) = 0;
+    plot(x, u, '-b.'); hold on;
+    plot(x1, u1, 'k')
+    
+end% for
+end% func
--- SWE1D/TransiteCellIdentify.m
+++ SWE1D/TransiteCellIdentify.m
@@ -1,10 +1,10 @@
-function refineflag = TransiteCellIdentify(mesh, h, bedElva)
+function transIndex = TransiteCellIdentify(mesh, h)
 % identify the wet/dey interface element
 % Input: 
 %   h - water depth
 %   bedElva - bottom elevation
 % Output:
-%   refineflag - bool flag for wet/dry refinement element, size [1, Ne]
+%   transIndex - bool flag for wet/dry transition element, size [1, Ne]
 
 % identify transitation element
 hPositive = 10^-3;
@@ -17,14 +17,5 @@ transIndex = xor(wetIndex(mesh.EToE(:, 1)), wetIndex(mesh.EToE(:, 2)));
 % transitation element shoule be wet
 transIndex = transIndex & wetIndex;
 
-% refinement condition
-hP = max( h(mesh.vmapP) );
-hmean = CellMean(mesh,h);
-refineflag = 2*hmean + hPositive < hP;
-% bedMean = CellMean(mesh, bedElva);
-% bedMax = max(bedElva);
-% refineflag = (hmean + bedMean + eps) < (bedMax);
-
-refineflag = refineflag(:) & transIndex(:);
 end% function 
 
