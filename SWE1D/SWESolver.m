bcc34043001135b1328a4babcd7bbfd0881250ae
li12242 <li12242@tju.edu.cn>
2016-04-21 17:00:40 +0800

    Bug fixed

--- SWE1D/SWESolver.m
+++ SWE1D/SWESolver.m
@@ -3,32 +3,16 @@ function [h, q] = SWESolver(physics, ncfile)
 % 
 mesh = physics.getVal('mesh');
 bedElva = physics.getVal('bedElva');
-
-rk4a = [            0.0 ...
-        -567301805773.0/1357537059087.0 ...
-        -2404267990393.0/2016746695238.0 ...
-        -3550918686646.0/2091501179385.0  ...
-        -1275806237668.0/842570457699.0];
-rk4b = [ 1432997174477.0/9575080441755.0 ...
-         5161836677717.0/13612068292357.0 ...
-         1720146321549.0/2090206949498.0  ...
-         3134564353537.0/4481467310338.0  ...
-         2277821191437.0/14882151754819.0];
-rk4c = [             0.0  ...
-         1432997174477.0/9575080441755.0 ...
-         2526269341429.0/6820363962896.0 ...
-         2006345519317.0/3224310063776.0 ...
-         2802321613138.0/2924317926251.0];
      
 time = 0;
-q = physics.getVal('flux'); h = physics.getVal('height');
+q = physics.getVal('flux'); 
+h = physics.getVal('height');
+[rk4a, rk4b, rk4c] = RKcoeff;
 
-% compute time step size
+% compute initial time step size
 xmin = min(abs(mesh.x(1,:)-mesh.x(2,:)));
 CFL=0.3; outstep = 0;
 FinalTime = physics.getVal('FinalTime');
-
-lamda = SWESpeed(h, q); dt = CFL/lamda*xmin;
 
 % eliminate zero depth in wet cell
 [h, q] = PositivePreserving(mesh, h, q, bedElva);
@@ -36,7 +20,8 @@ lamda = SWESpeed(h, q); dt = CFL/lamda*xmin;
 % outer time step loop
 while(time<FinalTime)
     lamda = SWESpeed(h, q);
-
+    dt = CFL/lamda*xmin;
+    
     % Increment time
     if time + dt > FinalTime
         time = FinalTime;
@@ -44,12 +29,13 @@ while(time<FinalTime)
     else
         time = time + dt;
     end% if
-    
-    if lamda*dt > CFL*xmin
-        dt = dt/2;
-    elseif lamda*dt < CFL*xmin/4
-        dt = dt*2;
-    end%if
+
+%     
+%     if lamda*dt > CFL*xmin
+%         dt = dt/2;
+%     elseif lamda*dt < CFL*xmin/4
+%         dt = dt*2;
+%     end%if
 
     fprintf('Processing: %f, dt: %f, wave speed: %f\n',...
         time./FinalTime, dt, lamda)
@@ -152,4 +138,23 @@ TOL = 0.03; g = 9.8;
 flag = (h>TOL);
 u = (q./h) + sqrt(g*h);
 lambda = max(u(flag));
+end% func
+
+function [rk4a, rk4b, rk4c] = RKcoeff
+% get Runge-Kutta coefficients
+rk4a = [            0.0 ...
+        -567301805773.0/1357537059087.0 ...
+        -2404267990393.0/2016746695238.0 ...
+        -3550918686646.0/2091501179385.0  ...
+        -1275806237668.0/842570457699.0];
+rk4b = [ 1432997174477.0/9575080441755.0 ...
+         5161836677717.0/13612068292357.0 ...
+         1720146321549.0/2090206949498.0  ...
+         3134564353537.0/4481467310338.0  ...
+         2277821191437.0/14882151754819.0];
+rk4c = [             0.0  ...
+         1432997174477.0/9575080441755.0 ...
+         2526269341429.0/6820363962896.0 ...
+         2006345519317.0/3224310063776.0 ...
+         2802321613138.0/2924317926251.0];
 end% func
