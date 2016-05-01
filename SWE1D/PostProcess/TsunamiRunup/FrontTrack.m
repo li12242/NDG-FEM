dc3d61cc1f52a4cbb18fb2be04f26ee82e44eac4
li12242 <li12242@tju.edu.cn>
2016-05-01 20:37:23 +0800

    Post process of TsunamiRunup

--- SWE1D/PostProcess/TsunamiRunup/FrontTrack.m
+++ SWE1D/PostProcess/TsunamiRunup/FrontTrack.m
@@ -1,69 +1,42 @@
-function [time, position] = FrontTrack
+function [time, rp, rm] = FrontTrack
 % get the wet/dry front position
 
+hm = 1e-1;
+hp = hm;
+
 % output file
-filename = 'SWE1DTsunamiRunup.nc';
+filename = 'SWE1D.nc';
 
-% rebuild element variables
-x1 = -500; x2 = 50000;
-np = 2; nele = 1000;
-[~, VX, ~, EToV] = Utilities.Mesh.MeshGen1D(x1, x2, nele);
-line = StdRegions.Line(np-1);
-mesh = MultiRegions.RegionLine(line, EToV, VX);
-
+% rebuild mesh
+x = ncread(filename, 'x'); x = reshape(x, 2, numel(x)/2);
 
 % get results
-time = ncread(filename, 'time'); x = ncread(filename, 'x');
-itime = 1;
-h = ncread(filename, 'h', [1, itime],[inf, 1]);
-
-
-position = zeros(size(time));
-transIndex = TransiteCellIdentify(mesh, h);
-position(itime) = getFrontPos(mesh, h, transIndex);
-
-% % bottom topography
-% bedElva = 5000 - 0.1*x;
-
-for itime = 1:numel(time)
-    h = ncread(filename, 'h', [1, itime],[inf, 1]);
-
-    transIndex = TransiteCellIdentify(mesh, h);
-    position(itime) = getFrontPos(mesh, h, transIndex);
-
-%     if (numel(transIndex) > 1) | (isempty(transIndex))
-%         error('transition element number error.')
-%     end% if
+time = ncread(filename, 'time');
+rp = zeros(size(time));
+rm = zeros(size(time));
+parfor itime = 1:numel(time)
+    h = ncread(filename, 'h', [1, itime],[inf, 1]); h = reshape(h, 2, numel(x)/2);
+    rp(itime) = getFrontP(x, h, hp);
+    rm(itime) = getFrontM(x, h, hm);
+    
     fprintf('Processing: %f ...\n', itime/numel(time))
 end% for
+
+% load exact interface position
+load('Shoreline.mat');
+figure
+plot(pt, px, 'k'); hold on;
+plot(time, rp, 'r.', time, rm, 'b.')
+t = legend('Exact', '$r^+$', '$r^-$');
+set(t, 'Box', 'off',  'Interpreter', 'Latex');
 end% func
 
-function xw = getFrontPos(mesh, h, index)
-nodeIndex = [index*2-1; index*2];
-vx = mesh.x(mesh.vmapM(:, index)); dx = vx(2) - vx(1);
-hmean = CellMean( mesh, h(nodeIndex) );
-hP = h(mesh.vmapP(:, index ));
-
-deltax = 2*(hmean*dx) ./(hP(2));
-xw = vx(1) + deltax;
+function rp = getFrontP(x, h, hDry)
+index = h > hDry;
+rp = min(x(index));
 end
 
-function transIndex = TransiteCellIdentify(mesh, h)
-% identify the wet/dey interface element
-% Input: 
-%   h - water depth
-%   bedElva - bottom elevation
-% Output:
-%   transIndex - bool flag for wet/dry transition element, size [1, Ne]
-
-% identify transitation element
-hPositive = 10^-3;
-% define wet cells
-iswet = (h > hPositive);
-wetIndex = (iswet(1:2:end-1) + iswet(2:2:end)) > 0;
-% when adjacent element possess different wet/dry status
-% transIndex assignment is true
-transIndex = xor(wetIndex(mesh.EToE(:, 1)), wetIndex(mesh.EToE(:, 2)));
-% transitation element shoule be wet
-transIndex = find(transIndex & wetIndex);
-end% function 
+function rm = getFrontM(x, h, hDry)
+index = h <= hDry;
+rm = max(x(index));
+end% func
