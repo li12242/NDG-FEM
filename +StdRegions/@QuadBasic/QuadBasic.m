141313a1849d6edd727e7df3ed5175fd444ad054
li12242 <li12242@tju.edu.cn>
2016-05-05 22:13:15 +0800

    standard element

---
+++ +StdRegions/@QuadBasic/QuadBasic.m
@@ -0,0 +1,51 @@
+classdef QuadBasic < StdRegions.BaseElement
+    % Stdandard Element Quadrilatral Basic
+    % QuadBasic properties(Inherit):
+    %   nDim        - the dimension of element 
+    %   nFace       - the number of edges 
+    %   nNode       - the number of nodes 
+    %   nOrder      - the order of degrees 
+    %   nVertice	- the number of vertices 
+    %   sName       - the name of element
+    % QuadBasic properties:
+    %   M           - Mass matrix
+    %   invM        - inverse of Mass Matrix
+    %   Dr          - Derivative Matrix of r
+    %   Ds          - Derivative Matrix of s
+    %   Drw         - Derivative Matrix of r in weak form 
+    %   Dsw         - Derivative Matrix of s in weak form 
+    %   r           - point coordinate of dim 1, [nNode x 1] 
+    %   s           - point coordinate of dim 2, [nNode x 1] 
+    %   VandMatrix  - Vandmonde Matrix
+    properties
+        M   % Mass matrix
+        invM    % inverse of Mass Matrix
+        Dr  % Derivative Matrix of r
+        Ds  % Derivative Matrix of s
+        Drw % Derivative Matrix of r in weak form 
+        Dsw % Derivative Matrix of s in weak form 
+        r   % point coordinate of dim 1, [nNode x 1] 
+        s   % point coordinate of dim 2, [nNode x 1] 
+        VandMatrix  % Vandmonde Matrix
+    end% properties
+    
+    methods
+        function obj = QuadBasic(nOrder)
+            import StdRegions.Quad.*
+            
+            dim = 2; vertice = 4; face = 4;
+            obj = obj@StdRegions.BaseElement(dim,vertice,nOrder,face);
+            obj.sName = 'Quad';
+            obj.nNode = (nOrder+1)^2;
+            
+            [obj.r, obj.s] = getCoor(nOrder);
+            obj.VandMatrix = getVandMatrix(obj.nOrder, obj.r, obj.s);
+            invV = inv(obj.VandMatrix);
+            obj.M = invV'*invV;
+            
+            obj.invM = obj.VandMatrix*(obj.VandMatrix)';
+            [obj.Dr, obj.Ds, obj.Drw, obj.Dsw] = ...
+                getDeriMatrix(obj.nOrder, obj.r, obj.s ,obj.VandMatrix);
+        end% func
+    end
+end% classdef
