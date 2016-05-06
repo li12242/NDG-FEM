0893fbc714b8fe828d63e5a78b1ac244a39528f5
li12242 <li12242@tju.edu.cn>
2016-05-06 21:25:52 +0800

    QuadMesh MultiRegion

--- +StdRegions/+Quad/getCoor.m
+++ +StdRegions/+Quad/getCoor.m
@@ -1,11 +1,12 @@
 function [r, s] = getCoor(nOrder)
 % get the coordinate of standard quadrilateral
+% coordinate 'r' comes first
 
-% get points distribution on edges
+% get points distribution on a edge
 np = nOrder+1;
 [x,~] = Polylib.zwglj(np);
 
-% coord 'r' fist; coord 's' second
+% coord 'r' counts fist; 's' is second
 r = x*ones(1, np);
 s = ones(np, 1)*x';
 
