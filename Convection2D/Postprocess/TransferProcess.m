2d2cb6efa1ba3c8843975bdd4e073e3c04c6149c
li12242 <li12242@tju.edu.cn>
2016-05-14 10:57:41 +0800

    finish JKTA limiter for triangle and quadrilateral shapes

--- Convection2D/Postprocess/TransferProcess.m
+++ Convection2D/Postprocess/TransferProcess.m
@@ -1,5 +1,5 @@
 function TransferProcess
-filename = 'Convection2D_2_40.nc';
+filename = 'Convection2D_2_20.nc';
 
 time = ncread(filename, 'time');
 % timestep = numel(time)
@@ -16,21 +16,22 @@ end% func
 function p_h = point3D(filename, x, y, var, time)
 ind = 1:6:numel(x);
 
-subplot(2, 1, 1);
+% subplot(2, 1, 1);
 p_h = plot3(x(ind), y(ind), var(ind), '.');
 zlim([-0.5, 1.2])
 
-subplot(2, 1, 2);
-dis = ncread(filename, 'temp', [1, 1], [inf, 1]);
+% subplot(2, 1, 2);
+% dis = ncread(filename, 'temp', [1, 1], [inf, 1]);
+% 
+% % contour plot
+% np = 100;
+% r = linspace(-1, 1, np);
+% [X, Y] = meshgrid(r, r);
+% Z = griddata(x(ind), y(ind), dis, X, Y, 'linear');
+% v = [0:0.2:1.2];
+% [~, c_h] = contourf(X, Y, Z, v);
+% colorbar
 
-% contour plot
-np = 100;
-r = linspace(-1, 1, np);
-[X, Y] = meshgrid(r, r);
-Z = griddata(x(ind), y(ind), dis, X, Y, 'linear');
-v = [0:0.2:1.2];
-[~, c_h] = contourf(X, Y, Z, v);
-colorbar
 % gather information
 % sumCell = zeros(numel(time), 1);
 
@@ -41,8 +42,8 @@ for itime = 1:numel(time)
     set(p_h, 'ZData', var(ind));
 %     set(p2_h, 'ZData', dis);
     
-    Z = griddata(x(ind), y(ind), dis, X, Y, 'linear');
-    set(c_h, 'ZData', Z);
+%     Z = griddata(x(ind), y(ind), dis, X, Y, 'linear');
+%     set(c_h, 'ZData', Z);
     drawnow;
 %     sumCell(itime) = sum(var(1:6));
     fprintf('Processing: %f ...\n', itime/numel(time))
