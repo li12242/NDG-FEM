54abb727328a5f7eea6cbe62934647663da0537b
li12242 <li12242@tju.edu.cn>
2016-06-29 00:35:55 +0800

    modify uniform mesh with different number of elements along each coordinate

--- +Utilities/+Mesh/MeshGenRectangle2D.m
+++ +Utilities/+Mesh/MeshGenRectangle2D.m
@@ -1,25 +1,51 @@
-function [EToV, VX, VY] = MeshGenRectangle2D(np, start_coor, end_coor)
+%% MeshGenRectangle2D
+% Generation uniform mesh of rectangle elements.
+% The domain is of [xmin, xmax]x[ymin, ymax], 
+
+function [EToV, VX, VY] = MeshGenRectangle2D(nx, ny, xmin, xmax, ymin, ymax)
 % Input:
-%   np - No of vertice on one edge
-%   [a, b] - interval
+%   nx     - No of vertice along x
+%   ny     - No of vertice along y
+%   xmin   - start position of x
+%   xmax   - end position of x
+%   ymin   - start position of y
+%   ymax   - end position of y
+% Output:
+%   EToV   - element to vertex list
+%   VX     - vertex coordinate
+%   VY     - vertex coordinate
+% 
+% Usages
+%   [E, X, Y] = MeshGenRectangle2D(2, 3, -1.0, 1.0, -1.0, 1.0)
+% 
 
-m = np - 1; % elements on each edge
+% Parameters
+mx   = nx - 1; % number of elements along x coordinate
+my   = ny - 1;
 
-VX = linspace(start_coor, end_coor, np); 
-VY = linspace(start_coor, end_coor, np)'; 
+%% Define vectex
+% The vertex is sorted along x coordinate. (x coordinate counts first)
+VX   = linspace(xmin, xmax, nx) ;
+VY   = linspace(ymin, ymax, ny)'; 
+VX   = repmat(VX, 1, ny) ;
+VY   = repmat(VY, 1, nx)'; 
+VX   = VX(:);
+VY   = VY(:);
 
-VX = repmat(VX, 1, np);
-VY = repmat(VY, 1, np)'; 
-VY = VY(:);
-
-EToV = zeros(m^2, 4);
-for irow = 1:m
-    for icol = 1:m
+%% Define EToV
+% The element is conuting along x coordinate
+EToV = zeros(mx*my, 4);
+for i = 1:my
+    for j = 1:mx
+        % element index
+        ind  = (i-1)*mx + j;
+        % vertex index
+        v1   = nx*(i-1) + j;
+        v2   = nx*(i-1) + j + 1;
+        v3   = nx*i     + j;
+        v4   = nx*i     + j + 1;
         % Counterclockwise
-        index = (irow-1)*m + icol;
-        EToV(index, :) =  ...
-            [np*(irow-1)+icol, np*(irow-1)+icol+1, ...
-            np*irow+icol+1, np*irow + icol, ];
+        EToV(ind,:)=[v1, v2, v4, v3];
     end% for
 end% for
 
--- SWE2D/Utilities/DrawProcess.m
+++ SWE2D/Utilities/DrawProcess.m
@@ -4,6 +4,32 @@ DrawSurface;
 
 end% func
 
+function DrawSection
+filename = 'SWE2D.nc';
+varname  = 'h';
+time     = ncread(filename, 'time');
+x        = ncread(filename, 'x');
+y        = ncread(filename, 'y');
+bot      = ncread(filename, 'bot');
+
+[np, ne] = size(x);
+ntime    = numel(time);
+
+%% Read result and interpolation
+figure('Position', [430, 375, 660, 429]);
+
+ist = 1;
+var = ncread(filename, varname, [1,1,ist], [np, ne, 1]);
+p   = plot3(x(:), y(:), bot(:) + var(:), '.');
+
+for ist = 1:ntime
+    var = ncread(filename, varname, [1,1,ist], [np, ne, 1]);
+    set(p, 'ZData', bot(:) + var(:));
+    drawnow; 
+    fprintf('Processing %f...\n', ist/ntime);
+end
+end% func
+
 function DrawSurface
 %% Parameters
 filename = 'SWE2D.nc';
@@ -26,8 +52,7 @@ p   = plot3(x(:), y(:), bot(:) + var(:), '.');
 for ist = 1:ntime
     var = ncread(filename, varname, [1,1,ist], [np, ne, 1]);
     set(p, 'ZData', bot(:) + var(:));
-    drawnow; 
-%     pause(0.1);
+    drawnow;
     fprintf('Processing %f...\n', ist/ntime);
 end
 end% func
