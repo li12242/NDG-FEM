39849b065a3d577ed066549f6731e6d7394e527d
li12242 <li12242@tju.edu.cn>
2016-08-14 21:09:39 +0800

    add comparison postprocess of sponge layer result

--- SWE1D/SWE_Driver1d.m
+++ SWE1D/SWE_Driver1d.m
@@ -16,7 +16,7 @@ caseName = 'LocalMovingMound';
 minDepth = 1e-4;
 g        = 9.81;
 n        = 1;   % polynomial order 
-nele     = 100; % No. of elements
+nele     = 1000; % No. of elements
 
 % Set to strucutre variable
 phys.name     = caseName;
