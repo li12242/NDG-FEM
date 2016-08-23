1f09c0e61382767015b14941f34ab8f0091fc83d
li12242 <li12242@tju.edu.cn>
2016-08-23 21:03:55 +0800

    smaller sponge layer

--- SWE1D/Utilities/SpongeLayer/timeDifferenceForSpongeLayer.m
+++ SWE1D/Utilities/SpongeLayer/timeDifferenceForSpongeLayer.m
@@ -9,19 +9,20 @@ order      = 1;
 coarseStru = Utilities.PostProcess.Postprocess(coarsefile, eletype, order);
 fineStru   = Utilities.PostProcess.Postprocess(finefile, eletype, order);
 
-
 % parameters
 fileID = 1;
 stime  = 12*3600;
 hC     = coarseStru.GetVarData('h', stime, fileID);
-% xC     = coarseStru.NcFile.GetVarData('x');
 
 timeF  = fineStru.NcFile.GetVarData('time');
 nt     = numel(timeF);
 xF     = fineStru.NcFile.GetVarData('x');
 err    = zeros(nt, 1);
 
+% interp from coarse gird to fine grid
 hCF    = coarseStru.Interp1d(hC, xF, fileID);
+
+% loop of the fine grid result, find the minimum error and time step
 for i = 1:nt
     hF     = fineStru.GetVarData('h', timeF(i), fileID);
     err(i) = Linf(hF, hCF);
@@ -33,6 +34,7 @@ end% for
 dt = timeF(ind) - stime;
 end% func
 
+%% subroutine function
 function err = Linf(y1, y2)
 err = max2( abs(y1-y2) );
 end% func
