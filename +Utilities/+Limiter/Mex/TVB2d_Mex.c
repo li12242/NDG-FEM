aa4eeaf93d172a90245057f946bd8bfdb6e018db
li12242 <li12242@tju.edu.cn>
2016-09-04 21:54:56 +0800

    diffusion TVB

--- +Utilities/+Limiter/Mex/TVB2d_Mex.c
+++ +Utilities/+Limiter/Mex/TVB2d_Mex.c
@@ -73,7 +73,7 @@ void mexFunction (int nlhs, mxArray *plhs[],
 	}
 	// calculate volume mean value
 	for(k=0;k<K;k++){
-		real *fld[3], cmean[3];
+		real *fld[3], cmean[3], face_len;
 		fld[0] = h+k*Np; fld[1] = x+k*Np; fld[2] = y+k*Np;
 		cellMean(Np, 3, fld, w, J+k*Np, cmean, area+k);
 		hmean[k] = cmean[0]; xmean[k] = cmean[1]; ymean[k] = cmean[2];
@@ -82,33 +82,29 @@ void mexFunction (int nlhs, mxArray *plhs[],
 	real *neigh_mean = (real*) malloc(sizeof(real)*Nfaces );
 	real *neigh_xc   = (real*) malloc(sizeof(real)*Nfaces );
 	real *neigh_yc   = (real*) malloc(sizeof(real)*Nfaces );
-	real *neigh_xf   = (real*) malloc(sizeof(real)*Nfaces );
-	real *neigh_yf   = (real*) malloc(sizeof(real)*Nfaces );
-	real *face_mean  = (real*) malloc(sizeof(real)*Nfaces );
+	real *xv         = (real*) malloc(sizeof(real)*Nfaces );
+	real *yv         = (real*) malloc(sizeof(real)*Nfaces );
+	// real *neigh_xf   = (real*) malloc(sizeof(real)*Nfaces );
+	// real *neigh_yf   = (real*) malloc(sizeof(real)*Nfaces );
+	// real *face_mean  = (real*) malloc(sizeof(real)*Nfaces );
 	real *delta 	 = (real*) malloc(sizeof(real)*Nfaces );
-
 	real alpha[2], a[4], df[2], v = 1.5;
 
 	for(k=0;k<K;k++){
 		real xc = xmean[k];
 		real yc = ymean[k];
+		real hc = hmean[k];
 
-		real *fv[3], fmean[3];
+		real *fv[3], face_len;
 		fv[0] = h+k*Np; fv[1] = x+k*Np; fv[2] = y+k*Np;
-		/* calculate average on face */
 		for(f=0;f<Nfaces;f++){
-			real face_len;
-			faceMean(Nfaces, Nfp, 3, fv, ws, sJ+k*Nfaces*Nfp+f*Nfp, Fmask+f, fmean, &face_len);
-			face_mean[f] = fmean[0]; neigh_xf[f]  = fmean[1]; neigh_yf[f]  = fmean[2];
-
-			// mexPrintf("k=%d, Nfaces=%d, xf=%f, yf=%f, face_len=%f, face_mean=%f\n",
-			// 		k, f, neigh_xf[f], neigh_yf[f], face_len, face_mean[f]);
-			// averages of adjacent elements
-			int e1 = (int)EToE[k+K*f] - 1;
-			if(e1 == k){ // for boundary
-				neigh_mean[f] = face_mean[f];
-				neigh_xc[f] = neigh_xf[f];
-				neigh_yc[f] = neigh_yf[f];
+			int e1 = (int) EToE[k+f*K] - 1;
+			if(e1==k){
+				real fmean[3];
+				faceMean(Nfaces, Nfp, 3, fv, ws, sJ+k*Nfaces*Nfp+f*Nfp, Fmask+f, fmean, &face_len);
+				neigh_mean[f] = fmean[0];
+				neigh_xc[f]   = fmean[1];
+				neigh_yc[f]   = fmean[2];
 			}else{
 				neigh_mean[f] = hmean[e1];
 				neigh_xc[f] = xmean[e1];
@@ -117,47 +113,94 @@ void mexFunction (int nlhs, mxArray *plhs[],
 		}
 
 		for(f1=0;f1<Nfaces;f1++){
-			/* find best other face */
-			real max_alpha = -1.0;
-			int  best_face = -1;
-			real beat_alpha[2];
+			int v  = k*Np + (int) (Fmask[f1]-1);
+			int f2 = (int) (f1+Nfaces-1)%Nfaces;
 
-			a[0]  = neigh_xc[f1] - xc;
-			a[2]  = neigh_yc[f1] - yc;
-			df[0] = neigh_xf[f1] - xc;
-			df[1] = neigh_yf[f1] - yc;
+			xv[f1] = x[v]; yv[f1] = y[v];
+			df[0] = x[v] - xc; df[1] = y[v] - yc;
+			a[0] = neigh_xc[f1] - xc; a[1] = neigh_xc[f2] - xc;
+			a[2] = neigh_yc[f1] - yc; a[3] = neigh_yc[f2] - yc;
 
-			for(f2=0;f2<Nfaces;f2++){
-				if(f1==f2)
-					continue;
+			matrixSolver2(a, df, alpha);
 
-				/* calculate of alpha */
-				a[1] = neigh_xc[f2] - xc;
-				a[3] = neigh_yc[f2] - yc;
+			real dhv = alpha[0]*(neigh_mean[f1]-hc)+alpha[1]*(neigh_mean[f2]-hc);
+			real dx2 = df[0]*df[0] + df[1]*df[1];
 
-				matrixSolver2(a, df, alpha);
-
-				real alpha_det = sqrt(alpha[0]*alpha[0] + alpha[1]*alpha[1]);
-				if ((alpha[0] > -TOTALERR) & (alpha[1] > -TOTALERR) & alpha[0]/alpha_det > max_alpha ){
-					best_face = f2;
-					max_alpha = alpha[0]/alpha_det;
-					beat_alpha[0] = alpha[0];
-					beat_alpha[1] = alpha[1];
-				}
-			}
-
-			real dhc, dhf, len;
-
-			dhc    = alpha[0]*(neigh_mean[f1] - hmean[k]) + alpha[1]*(neigh_mean[best_face] - hmean[k]);
-			dhf    = face_mean[f1] - hmean[k];
-			len    = sqrt( (neigh_xc[f1]-xc)*(neigh_xc[f1]-xc)+(neigh_yc[f1]-yc)*(neigh_yc[f1]-yc) );
-
-			delta[f1] = TVB_minmod(dhf, dhc, len, *factor);
-
-			// mexPrintf("k=%d, Nfaces=%d, f1=%d, xf=%f, yf=%f, delta=%f\n",
-					// k, f1, best_face, neigh_xf[f1], neigh_yf[f1], delta[f1]);
+			delta[f1] = TVB_minmod(h[v]-hc, dhv*v, dx2, *factor);
+			// mexPrintf("k=%d, v=%d, dh=%f, delta_h=%f, th=%f, delta=%f\n",
+					// k, f1, h[v]-hc, dhv*v, dx2*factor[0], delta[f1]);
 		}
-		/* correct the average to sum to 0.0 */
+
+
+	// for(k=0;k<K;k++){
+	// 	real xc = xmean[k];
+	// 	real yc = ymean[k];
+	//
+	// 	real *fv[3], fmean[3];
+	// 	fv[0] = h+k*Np; fv[1] = x+k*Np; fv[2] = y+k*Np;
+	// 	/* calculate average on face */
+	// 	for(f=0;f<Nfaces;f++){
+	// 		real face_len;
+	// 		faceMean(Nfaces, Nfp, 3, fv, ws, sJ+k*Nfaces*Nfp+f*Nfp, Fmask+f, fmean, &face_len);
+	// 		face_mean[f] = fmean[0]; neigh_xf[f]  = fmean[1]; neigh_yf[f]  = fmean[2];
+	//
+	// 		// mexPrintf("k=%d, Nfaces=%d, xf=%f, yf=%f, face_len=%f, face_mean=%f\n",
+	// 		// 		k, f, neigh_xf[f], neigh_yf[f], face_len, face_mean[f]);
+	// 		// averages of adjacent elements
+	// 		int e1 = (int)EToE[k+K*f] - 1;
+	// 		if(e1 == k){ // for boundary
+	// 			neigh_mean[f] = face_mean[f];
+	// 			neigh_xc[f] = neigh_xf[f];
+	// 			neigh_yc[f] = neigh_yf[f];
+	// 		}else{
+	// 			neigh_mean[f] = hmean[e1];
+	// 			neigh_xc[f] = xmean[e1];
+	// 			neigh_yc[f] = ymean[e1];
+	// 		}
+	// 	}
+	//
+	// 	for(f1=0;f1<Nfaces;f1++){
+	// 		/* find best other face */
+	// 		real max_alpha = -1.0;
+	// 		int  best_face = -1;
+	// 		real beat_alpha[2];
+	//
+	// 		a[0]  = neigh_xc[f1] - xc;
+	// 		a[2]  = neigh_yc[f1] - yc;
+	// 		df[0] = neigh_xf[f1] - xc;
+	// 		df[1] = neigh_yf[f1] - yc;
+	//
+	// 		for(f2=0;f2<Nfaces;f2++){
+	// 			if(f1==f2)
+	// 				continue;
+	//
+	// 			/* calculate of alpha */
+	// 			a[1] = neigh_xc[f2] - xc;
+	// 			a[3] = neigh_yc[f2] - yc;
+	//
+	// 			matrixSolver2(a, df, alpha);
+	//
+	// 			real alpha_det = sqrt(alpha[0]*alpha[0] + alpha[1]*alpha[1]);
+	// 			if ((alpha[0] > -TOTALERR) & (alpha[1] > -TOTALERR) & alpha[0]/alpha_det > max_alpha ){
+	// 				best_face = f2;
+	// 				max_alpha = alpha[0]/alpha_det;
+	// 				beat_alpha[0] = alpha[0];
+	// 				beat_alpha[1] = alpha[1];
+	// 			}
+	// 		}
+	//
+	// 		real dhc, dhf, len;
+	//
+	// 		dhc    = alpha[0]*(neigh_mean[f1] - hmean[k]) + alpha[1]*(neigh_mean[best_face] - hmean[k]);
+	// 		dhf    = face_mean[f1] - hmean[k];
+	// 		len    = sqrt( (neigh_xc[f1]-xc)*(neigh_xc[f1]-xc)+(neigh_yc[f1]-yc)*(neigh_yc[f1]-yc) );
+	//
+	// 		delta[f1] = TVB_minmod(dhf, dhc, len, *factor);
+	//
+	// 		// mexPrintf("k=%d, Nfaces=%d, f1=%d, xf=%f, yf=%f, delta=%f\n",
+	// 				// k, f1, best_face, neigh_xf[f1], neigh_yf[f1], delta[f1]);
+	// 	}
+	// 	/* correct the average to sum to 0.0 */
 		real sum = 0.0;
 		for(f=0;f<Nfaces;f++){
 			sum += delta[f];
@@ -186,35 +229,19 @@ void mexFunction (int nlhs, mxArray *plhs[],
 		real qpx = 0.0;
         real qpy = 0.0;
 
-		meanGradient(Nfaces, neigh_xf, neigh_yf, delta, xc, yc, hmean[k], &qpx, &qpy);
-		// mexPrintf("k=%d, phpx=%f, phpy=%f\n", k, qpx, qpy);
-        // for (f = 0; f < Nfaces; f++) {
-        //     /* vertex index */
-        //     int l1 = k*Np + (int) (Fmask[f + 0*Nfaces] - 1);
-        //     int l2 = k*Np + (int) (Fmask[f + (Nfp-1)*Nfaces] - 1);
-		//
-        //     /* mean value on edge */
-		//
-        //     real dx = (real) (x[l2] - x[l1]);
-        //     real dy = (real) (y[l2] - y[l1]);
-		//
-        //     qpx += (delta[f]+hmean[k]) * dy;
-        //     qpy -= (delta[f]+hmean[k]) * dx;
-        // }
-        // qpx /= area[k];
-        // qpy /= area[k];
-
-        getLocalVar(Np, hmean[k], xc, yc, x+k*Np, y+k*Np,
-			qpx, qpy, hlim+k*Np);
+		meanGradient(Nfaces, xv, yv, delta, xc, yc, hc, &qpx, &qpy);
+        getLocalVar(Np, hc, xc, yc, x+k*Np, y+k*Np, qpx, qpy, hlim+k*Np);
 
 	}
 
 	free(delta);
 	free(neigh_xc);
 	free(neigh_yc);
-	free(neigh_xf);
-	free(neigh_yf);
-	free(face_mean);
+	free(xv);
+	free(yv);
+	// free(neigh_xf);
+	// free(neigh_yf);
+	// free(face_mean);
 	free(neigh_mean);
 
 	free(hmean);
@@ -230,14 +257,17 @@ void mexFunction (int nlhs, mxArray *plhs[],
  * modified minmod function of TVB limiter.
  * m = TVB_minmod(a1, a2)
  */
-real TVB_minmod(real a1, real a2, real dx, real TVB_factor){
-	real m, v = 1.5;
-	real a[2];
-	if(abs(a1)<TVB_factor*dx*dx){
+real TVB_minmod(real a1, real a2, real dx2, real TVB_factor){
+	real m;
+	// mexPrintf("a1=%f, a2=%f, Th=%f\n", a1, a2, TVB_factor*dx2);
+	if(fabs(a1)<TVB_factor*dx2){
 		m = a1;
+		// mexPrintf("a1=%d, Th=%f\n", fabs(a1), TVB_factor*dx2);
 	}else{
+		real a[2];
 		a[0] = a1; a[1] = a2;
 		minmod(2, a, &m);
+		// mexPrintf("a1=%f, a2=%f, minmod=%f\n", a[0], a[1], m);
 	}
 	return m;
 }
