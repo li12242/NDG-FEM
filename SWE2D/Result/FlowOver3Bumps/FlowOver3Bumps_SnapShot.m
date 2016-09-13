b9d738cb58b0ad86ed8e79c7c81c045cbfff406d
li12242 <li12242@tju.edu.cn>
2016-09-13 22:58:53 +0800

    test case of flow over 3 bump

--- SWE2D/Result/FlowOver3Bumps/FlowOver3Bumps_SnapShot.m
+++ SWE2D/Result/FlowOver3Bumps/FlowOver3Bumps_SnapShot.m
@@ -1,19 +1,21 @@
-function PartialDamBreak2d_SnapShot
+function FlowOver3Bumps_SnapShot
 
 %% Parameter
-T        = 10;
-meshtype = 'tri';
+T        = 16;
+meshtype = 'quad';
 filename = {'SWE2D.nc'};
 order    = 1;
 PostproTri  = Utilities.PostProcess.Postprocess(filename, meshtype, order);
 fileID   = 1;
 time     = (0:0.1:1)*T;
-
 for i = 1:numel(time)
-    figure
-    PostproTri.Snapshot2D('h', time(i), fileID);
-    zlim([0, 11]);
-    view(30, 32);
+    figure; hold on;
+    bh = PostproTri.SnapshotConst2D('bot',fileID);
+    bot = get(bh, 'Verti')
+    ph = PostproTri.Snapshot2D('h', time(i), fileID);
+    set(bh, 'FaceColor', [0.4, 0.4, .4]);
+    zlim([0, 5]);
+    view(18, 24);
     zlabel('ˮλ (m)','FontSize', 14);
     xlabel('x (m)','FontSize', 14);
     ylabel('y (m)','FontSize', 14);
