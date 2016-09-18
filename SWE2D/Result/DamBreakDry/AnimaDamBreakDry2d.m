3c4dee7d8bbf3dceaa70a1388e154e751d4d5293
li12242 <li12242@tju.edu.cn>
2016-09-18 16:00:45 +0800

    TsuamiRunup

--- SWE2D/Result/DamBreakDry/AnimaDamBreakDry2d.m
+++ SWE2D/Result/DamBreakDry/AnimaDamBreakDry2d.m
@@ -1,22 +1,24 @@
-function PartialDamBreak2d_SnapShot
+function AnimaDamBreakDry2d
+%ANIMADAMBREAKDRY Summary of this function goes here
+%   Detailed explanation goes here
 
 %% Parameter
-T        = 10;
 meshtype = 'quad';
 filename = {'SWE2D.nc'};
 order    = 1;
 PostproTri  = Utilities.PostProcess.Postprocess(filename, meshtype, order);
 fileID   = 1;
-time     = [(1e-12:0.1:1)*T, T];
+time     = PostproTri.NcFile(1).GetVarData('time');
 
 for i = 1:numel(time)
-    figure
-    PostproTri.Snapshot2D('h', time(i), fileID);
+    PostproTri.Snapshot2D('h', time(i), fileID); drawnow;
     zlim([0, 11]);
-    view(30, 32);
+    view(14, 16);
     zlabel('ˮλ (m)','FontSize', 14);
     xlabel('x (m)','FontSize', 14);
     ylabel('y (m)','FontSize', 14);
-    box on
+    box on;
+    cla;
 end% for
-end% func
+
+end
