06837be9aa13e000ae09b5c9d6791cd8f2aaad8b
li12242 <li12242@tju.edu.cn>
2016-09-29 13:35:22 +0800

    add TVB limiter for triangles

--- +Utilities/+Limiter/+Limiter2D/TVB_tri2d.m
+++ +Utilities/+Limiter/+Limiter2D/TVB_tri2d.m
@@ -1,11 +1,11 @@
-function [ hlim ] = TVB2d( mesh, h, factor )
-%TVB2D Summary of this function goes here
+function [ hlim ] = TVB_tri2d( mesh, h, factor )
+%TVB_TRI2D Summary of this function goes here
 %   Detailed explanation goes here
 
 % factor = 1.0;
 shape = mesh.Shape;
 
-hlim  = Utilities.Limiter.Limiter2D.TVB2d_Mex...
+hlim  = Utilities.Limiter.Limiter2D.TVB_tri2d_Mex...
     (h, mesh.J, mesh.sJ, shape.M, shape.Fmask, ...
     mesh.EToE, shape.Mes, mesh.x, mesh.y, factor);
 end
