7c230484910ff81fa2b5815c9161af5f0ea27299
li12242 <li12242@tju.edu.cn>
2016-10-26 17:32:50 +0800

    numerical test

--- SWE2D/Result/ParabolicBowl/SectionProfile_ParaBowl2d.m
+++ SWE2D/Result/ParabolicBowl/SectionProfile_ParaBowl2d.m
@@ -9,8 +9,8 @@ T     = 2*pi/w;
 delta = 0.5;
 rmin  = -4000+delta; 
 rmax  =  4000-delta;
-ne    = 1000;   % number of exact solution
-np    = 100;    % number of interpolated solutions
+ne    = 100;   % number of exact solution
+np    = 40;    % number of interpolated solutions
 
 %% Get points coordinate
 xe    = zeros(ne, 1);
@@ -24,7 +24,8 @@ rp    = (xp.^2 + yp.^2);
 bp    = alpha*rp;
 
 %% spicific time
-timeFrac = (0:1/4:1);
+timeFrac = linspace(eps, 1-eps, 4);
+% timeFrac = (1/4:1/4:3/4);
 time     = timeFrac*T;
 timeStr  = cell(numel(time), 1);
 for i = 1:numel(time)
@@ -33,59 +34,71 @@ end
 
 %% Construct postprocess class
 meshtype = 'quad';
-% elenum   = [80, 100, 120, 160, 200];
-% filename = cell(numel(elenum), 1);
-% for i =1:numel(elenum)
-%     filename{i} = ['SWE2D_', meshtype, '_', num2str(elenum(i)), '.nc'];
-% end
-filename{1} = 'SWE2D_60.nc';
-fileID   = 1;
-% create post process class for quad
-PostproQuad = Utilities.PostProcess.Postprocess(filename, meshtype, 1);
+filename = {'SWE2D_ParabolicBowl_quad_60.nc'};
+Postpro1 = Utilities.PostProcess.Postprocess(filename, meshtype, 1);
 
+meshtype = 'tri';
+filename = {'SWE2D_ParabolicBowl_tri_60.nc'};
+Postpro2 = Utilities.PostProcess.Postprocess(filename, meshtype, 1);
+
+lineWidth = 2;
+markerSize = 7;
 for ist = 1:numel(time)
     varname = 'h';
     extH    = ParabolicBowl2d_ExtDepth(xe, ye, time(ist));
-    numSol  = PostproQuad.GetVarData(varname, time(ist), fileID);
-    numH    = PostproQuad.Interp2D(numSol, xp, yp, fileID);
+    numH1 = InterpSolution(Postpro1, varname, time(ist), xp, yp);
+    numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
     % draw water height
     figure('Color', 'w');
-    plot(ye, extH+be, 'k--'); hold on
-    plot(yp, numH+bp, 'r+');
-    plot(ye, be, 'k');
-    ylabel('$Elvation \, (m)$', 'Interpreter', 'Latex');
-    xlabel('y (m)', 'Interpreter', 'Latex');
-    title(timeStr{ist}, 'Interpreter', 'Latex');
-    t = legend('Exact', 'RKDG');
-    set(t, 'box', 'off');
+    plot(ye, extH+be, 'k--', 'LineWidth',lineWidth); hold on
+    plot(yp, numH1+bp, 'ro-', 'MarkerSize', markerSize,...
+        'LineWidth',lineWidth, 'MarkerFaceColor', 'r');
+    plot(yp, numH2+bp, 'bs-', 'MarkerSize', markerSize,...
+        'LineWidth',lineWidth, 'MarkerFaceColor', 'b');
+    plot(ye, be, 'k', 'LineWidth',lineWidth);
+    ylabel('水位 (m)', 'FontSize', 14);
+    xlabel('y (m)', 'FontSize', 14);
+    title(timeStr{ist}, 'FontSize', 14);
+    legend({'Exact', '四边形', '三角形'}, 'box', 'off','FontSize', 14);
     
-    % draw flux
+    % draw flux qx
     varname = 'qx';
     extH    = ParabolicBowl2d_ExtQx(xe, ye, time(ist));
-    numSol  = PostproQuad.GetVarData(varname, time(ist), fileID);
-    numH    = PostproQuad.Interp2D(numSol, xp, yp, fileID);
+    numH1 = InterpSolution(Postpro1, varname, time(ist), xp, yp);
+    numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
     figure('Color', 'w');
-    plot(ye, extH, 'k--'); hold on
-    plot(yp, numH, 'r+');
+    plot(ye, extH, 'k--', 'LineWidth',lineWidth); hold on
+    plot(yp, numH1, 'ro-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+        'MarkerFaceColor', 'r');
+    plot(yp, numH2, 'bs-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+        'MarkerFaceColor', 'b');
     ylim([-1.25, 1.25]);
-    ylabel('$\rm{Discharge} \, q_x \, (m^2/s)$', 'Interpreter', 'Latex');
-    xlabel('y (m)', 'Interpreter', 'Latex');
-    title(timeStr{ist}, 'Interpreter', 'Latex');
-    t = legend('Exact', 'RKDG');
-    set(t, 'box', 'off');
+    ylabel('流量 q_x (m^2/s)', 'FontSize', 14);
+    xlabel('y (m)', 'FontSize', 14);
+    title(timeStr{ist}, 'FontSize', 14);
+    legend({'Exact', '四边形', '三角形'}, 'box', 'off','FontSize', 14);
     
-    % draw flux
+    % draw flux qy
     varname = 'qy';
     extH    = ParabolicBowl2d_ExtQy(xe, ye, time(ist));
-    numSol  = PostproQuad.GetVarData(varname, time(ist), fileID);
-    numH    = PostproQuad.Interp2D(numSol, xp, yp, fileID);
+    numH1 = InterpSolution(Postpro1, varname, time(ist), xp, yp);
+    numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
     figure('Color', 'w');
-    plot(ye, extH, 'k--'); hold on
-    plot(yp, numH, 'r+');
+    plot(ye, extH, 'k--', 'LineWidth',lineWidth); hold on
+    plot(yp, numH1, 'ro-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+        'MarkerFaceColor', 'r');
+    plot(yp, numH2, 'bs-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+        'MarkerFaceColor', 'b');
     ylim([-1.25, 1.25]);
-    ylabel('$\rm{Discharge} \, q_y \, (m^2/s)$', 'Interpreter', 'Latex');
-    xlabel('y (m)', 'Interpreter', 'Latex');
-    title(timeStr{ist}, 'Interpreter', 'Latex');
-    t = legend('Exact', 'RKDG');
-    set(t, 'box', 'off');
+    ylabel('流量 q_y (m^2/s)', 'FontSize', 14);
+    xlabel('y (m)', 'FontSize', 14);
+    title(timeStr{ist}, 'FontSize', 14);
+    legend({'Exact', '四边形', '三角形'}, 'box', 'off', 'FontSize', 14);
 end% for
+end% func
+
+function interpSol = InterpSolution(Postpro, varname, time, xp, yp)
+fileID  = 1;
+numSol  = Postpro.GetVarData(varname, time, fileID);
+interpSol= Postpro.Interp2D(numSol, xp, yp, fileID);
+end
