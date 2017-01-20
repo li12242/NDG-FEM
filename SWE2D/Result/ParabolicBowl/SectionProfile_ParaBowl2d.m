7087dd4ee9e4c45e554650cdf4c855633e0e64dc
li12242 <li12242@tju.edu.cn>
2017-01-20 14:27:16 +0800

    postprocess for quadrilateral swe model

--- SWE2D/Result/ParabolicBowl/SectionProfile_ParaBowl2d.m
+++ SWE2D/Result/ParabolicBowl/SectionProfile_ParaBowl2d.m
@@ -24,76 +24,82 @@ rp    = (xp.^2 + yp.^2);
 bp    = alpha*rp;
 
 %% spicific time
-timeFrac = linspace(eps, 1-eps, 4);
+timeFrac = linspace(eps, 1-eps, 6);
 % timeFrac = (1/4:1/4:3/4);
 time     = timeFrac*T;
 timeStr  = cell(numel(time), 1);
 for i = 1:numel(time)
-    timeStr{i} = ['t=',num2str(timeFrac(i)),'T',];
+    timeStr{i} = ['t = ',num2str(timeFrac(i)),'T',];
 end
 
 %% Construct postprocess class
 meshtype = 'quad';
-filename = {'SWE2D_ParabolicBowl_quad_60.nc'};
+filename = {'swe2d_quad_1_140.0-1.nc'};
 Postpro1 = Utilities.PostProcess.Postprocess(filename, meshtype, 1);
 
-meshtype = 'tri';
-filename = {'SWE2D_ParabolicBowl_tri_60.nc'};
-Postpro2 = Utilities.PostProcess.Postprocess(filename, meshtype, 1);
+% meshtype = 'quad';
+% filename = {'swe2d_quad_1_81.0-1.nc'};
+% Postpro2 = Utilities.PostProcess.Postprocess(filename, meshtype, 1);
 
 lineWidth = 2;
-markerSize = 7;
+markerSize = 5;
+fontSize = 20;
+dt = -20;
+
+for i = 2:numel(time)-1
+    time(i) = time(i) + dt;
+end
 for ist = 1:numel(time)
     varname = 'h';
     extH    = ParabolicBowl2d_ExtDepth(xe, ye, time(ist));
-    numH1 = InterpSolution(Postpro1, varname, time(ist), xp, yp);
-    numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
+    numH1 = InterpSolution(Postpro1, varname, time(ist)+dt, xp, yp);
+%     numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
     % draw water height
     figure('Color', 'w');
-    plot(ye, extH+be, 'k--', 'LineWidth',lineWidth); hold on
-    plot(yp, numH1+bp, 'ro-', 'MarkerSize', markerSize,...
+    plot(ye, extH+be, 'b-', 'LineWidth',lineWidth); hold on
+    plot(yp, numH1+bp, 'ro--', 'MarkerSize', markerSize,...
         'LineWidth',lineWidth, 'MarkerFaceColor', 'r');
-    plot(yp, numH2+bp, 'b^-', 'MarkerSize', markerSize,...
-        'LineWidth',lineWidth, 'MarkerFaceColor', 'b');
+%     plot(yp, numH2+bp, 'b^-', 'MarkerSize', markerSize,...
+%         'LineWidth',lineWidth, 'MarkerFaceColor', 'b');
     plot(ye, be, 'k', 'LineWidth',lineWidth);
-    ylabel('\eta (m)', 'FontSize', 14);
-    xlabel('y (m)', 'FontSize', 14);
-    title(timeStr{ist}, 'FontSize', 14);
-    legend({'Exact', 'Quad', 'Tri'}, 'box', 'off','FontSize', 14);
+    ylabel('$\eta (m)$', 'Interpreter', 'latex', 'FontSize', fontSize);
+    xlabel('$y (m)$', 'Interpreter', 'latex', 'FontSize', fontSize);
+%     title(timeStr{ist}, 'Interpreter', 'latex', 'FontSize', fontSize);
+    legend({'Exact', 'Numerical'}, 'box', 'off','FontSize', fontSize);
     
     % draw flux qx
     varname = 'qx';
     extH    = ParabolicBowl2d_ExtQx(xe, ye, time(ist));
-    numH1 = InterpSolution(Postpro1, varname, time(ist), xp, yp);
-    numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
+    numH1 = InterpSolution(Postpro1, varname, time(ist)+dt, xp, yp);
+%     numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
     figure('Color', 'w');
-    plot(ye, extH, 'k--', 'LineWidth',lineWidth); hold on
-    plot(yp, numH1, 'ro-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+    plot(ye, extH, 'b-', 'LineWidth',lineWidth); hold on
+    plot(yp, numH1, 'ro--', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
         'MarkerFaceColor', 'r');
-    plot(yp, numH2, 'b^-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
-        'MarkerFaceColor', 'b');
+%     plot(yp, numH2, 'b^-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+%         'MarkerFaceColor', 'b');
     ylim([-1.25, 1.25]);
-    ylabel('q_x (m^2/s)', 'FontSize', 14);
-    xlabel('y (m)', 'FontSize', 14);
-    title(timeStr{ist}, 'FontSize', 14);
-    legend({'Exact', 'Quad', 'Tri'}, 'box', 'off','FontSize', 14);
+    ylabel('$q_x (m^2/s)$', 'Interpreter', 'latex','FontSize', fontSize);
+    xlabel('$y (m)$', 'Interpreter', 'latex','FontSize', fontSize);
+    %title(timeStr{ist}, 'Interpreter', 'latex', 'FontSize', fontSize);
+    %legend({'Exact', 'Numerical'}, 'box', 'off','FontSize', fontSize);
     
     % draw flux qy
     varname = 'qy';
     extH    = ParabolicBowl2d_ExtQy(xe, ye, time(ist));
-    numH1 = InterpSolution(Postpro1, varname, time(ist), xp, yp);
-    numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
+    numH1 = InterpSolution(Postpro1, varname, time(ist)+dt, xp, yp);
+%     numH2 = InterpSolution(Postpro2, varname, time(ist), xp, yp);
     figure('Color', 'w');
-    plot(ye, extH, 'k--', 'LineWidth',lineWidth); hold on
-    plot(yp, numH1, 'ro-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+    plot(ye, extH, 'b-', 'LineWidth',lineWidth); hold on
+    plot(yp, numH1, 'ro--', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
         'MarkerFaceColor', 'r');
-    plot(yp, numH2, 'bs-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
-        'MarkerFaceColor', 'b');
+%     plot(yp, numH2, 'bs-', 'MarkerSize', markerSize, 'LineWidth',lineWidth,...
+%         'MarkerFaceColor', 'b');
     ylim([-1.25, 1.25]);
-    ylabel('q_y (m^2/s)', 'FontSize', 14);
-    xlabel('y (m)', 'FontSize', 14);
-    title(timeStr{ist}, 'FontSize', 14);
-    legend({'Exact', 'Quad', 'Tri'}, 'box', 'off', 'FontSize', 14);
+    ylabel('$q_y (m^2/s)$', 'Interpreter', 'latex', 'FontSize', fontSize);
+    xlabel('$y (m)$', 'Interpreter', 'latex', 'FontSize', fontSize);
+    %title(timeStr{ist}, 'Interpreter', 'latex', 'FontSize', fontSize);
+    %legend({'Exact', 'Numerical'}, 'box', 'off', 'FontSize', fontSize);
 end% for
 end% func
 
