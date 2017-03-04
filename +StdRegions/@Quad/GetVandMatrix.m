a8f8ab3c6cb76bc8517329804f63154578611094
Guoquan Ran <rgq1111@tju.edu.cn>
2017-03-04 20:52:58 +0800

    Delete Files

--- +StdRegions/@Quad/GetVandMatrix.m
+++
@@ -1,9 +0,0 @@
-function Va = GetVandMatrix(obj, order, r, s)
-% GETVANDMATRIX ¼ÆËãVandermonde¾ØÕó
-% Vandermonde¾ØÕóÔªËØÂú×ã V_{ij} = \psi_j(\xi_i)
-
-Va = zeros(numel(r), obj.nNode);
-
-for ind = 1:obj.nNode
-    Va(:, ind) = OrthogonalFun(r,s,order,ind);
-end
