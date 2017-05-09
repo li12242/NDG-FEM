d2c29546a055e8ca49765dadf4ea1e5f5fffbc31
li12242 <li12242@tju.edu.cn>
2017-05-09 21:36:02 +0800

    refactor netcdf class

---
+++ +ndg_utility/+nc/@nc_file/declare_file.m
@@ -0,0 +1,17 @@
+function declare_file( obj )
+%DECLARE_FILE Create a new NetCDF file and define contained variables
+%   Detailed explanation goes here
+
+obj.ncid = netcdf.create(obj.name,'CLOBBER');
+for n = 1:numel(obj.dims)
+    obj.dims(n).define_in_ncfile(obj.ncid);
+end
+
+for n = 1:numel(obj.vars)
+    obj.vars(n).define_in_ncfile(obj.ncid);
+end
+netcdf.endDef(obj.ncid);
+obj.isopen = true;
+
+end
+
