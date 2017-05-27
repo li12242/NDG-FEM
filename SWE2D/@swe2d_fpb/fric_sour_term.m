7ece7255d05d26a6949ccf6ff6fe4245b3e40479
li12242 <li12242@tju.edu.cn>
2017-05-27 23:07:09 +0800

    refactor well balanced

--- SWE2D/@swe2d_fpb/fric_sour_term.m
+++ SWE2D/@swe2d_fpb/fric_sour_term.m
@@ -3,10 +3,9 @@ function [ sf ] = fric_sour_term( obj, f_Q )
 %   Detailed explanation goes here
 
 sf(:,:,1) = zeros(obj.mesh.cell.Np, obj.mesh.K);
-u = sqrt(f_Q(:,:,2).^2 + f_Q(:,:,3).^2);
-
-sf(:,:,2) = -obj.tau.*u;
-sf(:,:,3) = -obj.tau.*u;
+u = sqrt( f_Q(:, :, 2).^2 + f_Q(:, :, 3).^2 );
+sf(:,:,2) = -obj.k.*f_Q(:,:,2).*u./f_Q(:,:,1).^2;
+sf(:,:,3) = -obj.k.*f_Q(:,:,3).*u./f_Q(:,:,1).^2;
 
 sf(:, ~obj.wetflag, 2) = 0;
 sf(:, ~obj.wetflag, 3) = 0;
